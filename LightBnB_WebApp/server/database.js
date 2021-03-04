8349b5b56141fac1d36b075f5a36dd4ea6c2dd8b
Sandra To <sandra.to.e@gmail.com>
2021-03-04 20:33:13 +0000

    Working code retrieves reservation with undefined and NaN values

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -71,8 +71,24 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  // return getAllProperties(null, 2);
+  const queryString = `
+    SELECT properties.*, reservations.*, AVG(rating) as average_rating
+    FROM reservations
+    JOIN properties ON property_id = properties.id
+    JOIN property_reviews ON reservations.id = reservation_id
+    WHERE reservations.guest_id = $1
+      AND end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY start_date
+    LIMIT $2;
+    `;
+
+  return pool
+    .query(queryString, [guest_id, limit])
+    .then(res => res.rows[0])
+    .catch(err => console.log(err));
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
