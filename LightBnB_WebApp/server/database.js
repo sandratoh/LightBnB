d6004732686ea72b199b277a1ef870196d967e32
Sandra To <sandra.to.e@gmail.com>
2021-03-04 19:38:20 +0000

    Replace user id query with sql instead of fake codes

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -34,8 +34,15 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+  return pool
+    .query(`
+    SELECT *
+    FROM users
+    WHERE id = $1
+    `, [id])
+    .then(res => res.rows[0])
+    .catch(err => console.log(err));
+};
 exports.getUserWithId = getUserWithId;
 
 
