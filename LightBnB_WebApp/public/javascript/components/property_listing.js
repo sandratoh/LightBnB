897386d0e36b3f6661b7f2d470dfaf7601ff88cd
Sandra To <sandra.to.e@gmail.com>
2021-03-05 03:00:23 +0000

    Working reservation form

--- LightBnB_WebApp/public/javascript/components/property_listing.js
+++ LightBnB_WebApp/public/javascript/components/property_listing.js
@@ -1,7 +1,7 @@
 $(() => {
   window.propertyListing = {};
   
-  function createListing(property, isReservation) {
+  const createListing = (property, isReservation) => {
     return `
     <article class="property-listing">
         <section class="property-listing__preview-image">
@@ -14,13 +14,25 @@ $(() => {
             <li>number_of_bathrooms: ${property.number_of_bathrooms}</li>
             <li>parking_spaces: ${property.parking_spaces}</li>
           </ul>
-          ${isReservation ? 
+          ${isReservation ?
             `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>` 
             : ``}
-          <footer class="property-listing__footer">
+            <footer class="property-listing__footer">
             <div class="property-listing__rating">${Math.round(property.average_rating * 100) / 100}/5 stars</div>
             <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
-          </footer>
+            <br>
+            <form class="reservation-form">
+              <div class="reservation-form-field">
+                <input type="date" name="start_date" placeholder="start date">
+              </div>
+              <div class="reservation-form-field">
+                <input type="date" name="end_date" placeholder="end date">
+              </div>
+              <div class="reservation-form-button">
+                <button type="submit">Make Reservation</button>
+              </div>
+            </form>
+            </footer>
         </section>
       </article>
     `
