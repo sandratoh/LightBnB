a46135037d97dc341046180f091ac933d94fc877
Sandra To <sandra.to.e@gmail.com>
2021-03-05 07:27:51 +0000

    Add route to post reservations

--- LightBnB_WebApp/server/apiRoutes.js
+++ LightBnB_WebApp/server/apiRoutes.js
@@ -2,11 +2,11 @@ module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
     database.getAllProperties(req.query, 20)
-    .then(properties => res.send({properties}))
-    .catch(e => {
-      console.error(e);
-      res.send(e)
-    }); 
+      .then(properties => res.send({properties}))
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   router.get('/reservations', (req, res) => {
@@ -16,11 +16,23 @@ module.exports = function(router, database) {
       return;
     }
     database.getAllReservations(userId)
-    .then(reservations => res.send({reservations}))
-    .catch(e => {
-      console.error(e);
-      res.send(e)
-    });
+      .then(reservations => res.send({reservations}))
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
+  });
+
+  router.post('/reservations', (req, res) => {
+    const userId = req.session.userId;
+    database.makeReservation({...req.body, owner_id: userId})
+      .then(reservation => {
+        res.send(reservation);
+      })
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   router.post('/properties', (req, res) => {
@@ -31,7 +43,7 @@ module.exports = function(router, database) {
       })
       .catch(e => {
         console.error(e);
-        res.send(e)
+        res.send(e);
       });
   });
 
