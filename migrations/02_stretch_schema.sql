941ed57a781d9dc2bb769b65943f1f15eb67213b
Sandra To <sandra.to.e@gmail.com>
2021-03-05 02:16:06 +0000

    Create new tables for stretch activities

---
+++ migrations/02_stretch_schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS guest_reviews CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY
+  , start_date DATE NOT NULL
+  , end_date DATE NOT NULL
+  , cost_per_night INTEGER NOT NULL
+  , property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+);
+
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY
+  , owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+  , guest_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+  , property_id INTEGER NOT NULL REFERENCES properties(id) ON DELETE CASCADE
+  , reservation_id INTEGER NOT NULL REFERENCES reservations(id) ON DELETE CASCADE
+  , rating SMALLINT NOT NULL DEFAULT 0
+  , message TEXT
+);
